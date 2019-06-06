d08dcede45ece5c017037a305e79d3b6599d37f8
Damir Cosic <d.cosic@sportradar.com>
2019-06-06 11:50:57 +0200

    Sign out implemented

---
+++ src/components/SidePanel/UserPanel.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+import firebase from '../../firebase';
+
+class UserPanel extends Component {
+    
+    dropDownOptions = () => [
+        {
+            key: 'user',
+            text: <span>Signed in as <strong>User</strong></span>,
+            disabled: true
+        },
+        {
+            key: 'avatar',
+            text: <span>Change Avatar</span>
+        },
+        {   key: 'signout',
+            text: <span onClick={this.handleSignout}>Sign Out</span>
+        },
+    ];
+
+    handleSignout = () => {
+        firebase
+        .auth()
+        .signOut()
+        .then(() => console.log('signout'))
+    }
+
+    render(){
+        return (
+            <Grid style={{background: '#4c3c4c'}}>
+                <Grid.Column>
+                    <Grid.Row style={{padding: '1.2em', margin: 0}}>
+                        {/* header */}
+                        <Header inverted floated="left" as="h2">    
+                            <Icon  name="code"/>
+                            <Header.Content >
+                                DevChat
+                            </Header.Content>
+                        </Header>
+                    </Grid.Row>
+                    {/* user drops down */}
+                    <Header style={{ padding: '0.25em'}} as="h4" inverted>
+                        <Dropdown trigger={<span>Users</span>} options={this.dropDownOptions()}/>
+                    </Header>
+                </Grid.Column>
+          
+            </Grid>
+        )
+    }
+}
+export default UserPanel;
